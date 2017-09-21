f2d3f15ac0c4f074e21d636febeb80a862028099
prodotiscus <f.sizov@yandex.ru>
2017-09-21 09:18:43 +0300

    resource handler ++

--- resource_handler/__init__.py
+++ resource_handler/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-marked_classes = {}
+marked_functions = {}
 
 
 def mark_as(identifiers):
@@ -11,7 +11,55 @@ def mark_as(identifiers):
     return wrapper
 
 
-@mark_as(['*'])
-class main:
+class Prepare:
+
+    def __init__(self, *args):
+        self.called = []
+        for tag_ in args:
+            for identifiers in marked_classes:
+                if tag_ in identifiers and not marked_classes[identifiers] in self.called:
+                    self.called.append(marked_classes[identifiers])
+
+        if len(self.called) == 0:
+            raise NoCalledClassesFound()
+
+    def get_parameter(self, key, element):
+        for called in self.called:
+            pass
+
+
+class Void:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_parameter(key, element):
+        return False
+
+
+def tag(strings):
+    def wrapper(fn):
+        def wrapped(*args):
+            if not tuple(strings) in marked_functions:
+                marked_functions[tuple(strings)] = [fn]
+            else:
+                marked_functions[tuple(strings)].append(fn)
+        return wrapped
+    return wrapper
+
+def provides(parameters):
+    def wrapper(fn):
+        def wrapped(*args):
+            #for
+
+
+def makeitalic(fn):
+    def wrapper(fn):
+        return "<i>" + fn() + "</i>"
+
+    return wrapped
+
+
+class NoCalledClassesFound(Exception):
     pass
-    # ...
+
