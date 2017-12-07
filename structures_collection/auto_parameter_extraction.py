d3b435f2edf68bee5848900bf4d90dc4e17a9eca
Fyodor Sizov <f.sizov@yandex.ru>
2017-12-07 16:15:51 +0300

    upd

--- structures_collection/auto_parameter_extraction.py
+++ structures_collection/auto_parameter_extraction.py
@@ -1,15 +1,43 @@
 #!/usr/bin/python3
 
 
-class Handler:
+class HandlerStart:
     def __init__(self):
-        pass
+        self.param_extractors = {}
 
-    def get_param_lambdas(self, system_name):
-        # return Arr [param_name, Func for system_name Providing param_name
-        pass
+    def get_param_extractor(self, system_name, param_name):
+        if (system_name, param_name) in self.param_extractors:
+            return self.param_extractors[(system_name, param_name)]
+        else:
+            raise NoSuchExtractor()
+
+    def add_param_extractor(self, system_name, param_name, func):
+        self.param_extractors[(system_name, param_name)] = func
+
+
+# every function should return List
+
+
+Handler = HandlerStart()
+
+
+def extract_parameter(system_name, param_name):
+
+    def segm_decorator(func):
+        Handler.add_param_extractor(system_name, param_name, func)
+
+        def wrapped(function_arg1):
+            return func(function_arg1)
+
+        return wrapped
+
+    return segm_decorator
 
 
 @extract_parameter('universal:token', 'universal:length')
 def length_of_token(content):
     return len(content)
+
+
+class NoSuchExtractor(Exception):
+    pass
--- structures_collection/auto_segmentation.py
+++ structures_collection/auto_segmentation.py
@@ -1,16 +1,47 @@
 #!/usr/bin/python3
 
 
-class Handler:
+class HandlerStart:
     def __init__(self):
-        pass
+        self.segments = {}
 
     def segment(self, from_, to_):
-        return True
+        if (from_, to_) in self.segments:
+            return self.segments[(from_, to_)]
+        else:
+            raise NoSuchSegmentTemplate()
+
+    def add_segment(self, from_, to_, func):
+        self.segments[(from_, to_)] = func
 
 
 # every function should return List
 
+
+Handler = HandlerStart()
+
+
+def segmentation(from_, to_):
+
+    def segm_decorator(func):
+        Handler.add_segment(from_, to_, func)
+
+        def wrapped(function_arg1):
+            return func(function_arg1)
+
+        return wrapped
+
+    return segm_decorator
+
+
 @segmentation('universal:input', 'universal:token')
 def input_to_tokens(content, metadata=None):
     return content.split()
+
+
+a = Handler.segment('universal:input', 'universal:token')('foo bar abc')
+print(a)
+
+
+class NoSuchSegmentTemplate(Exception):
+    pass
