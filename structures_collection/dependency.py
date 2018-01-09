54ef719a498d4ace4aac53d04b4b96d777064170
Fyodor Sizov <f.sizov@yandex.ru>
2018-01-09 22:30:38 +0300

    dependency extended

--- structures_collection/dependency.py
+++ structures_collection/dependency.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 
 systems = {
-    'universal:input': ['universal:token', 'universal:collocation'],
-    'universal:token': ['universal:morpheme', 'universal:char', 'universal:char_regex'],
-    'universal:morpheme': ['universal:char', 'universal:char_regex']
+    'universal:input': [
+        'universal:token',
+        'universal:collocation'
+    ],
+    'universal:token': [
+        'universal:morpheme',
+        'mansi:morphemeYU',
+        'mansi:VowMorpheme',
+        'universal:char',
+        'universal:char_regex'
+    ],
+    'universal:morpheme': [
+        'universal:char',
+        'universal:char_regex'
+    ],
+    'mansi:VowMorpheme': ['universal:morpheme'],
+    'mansi:morphemeYU': ['universal:morpheme']
 }
