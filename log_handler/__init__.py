712c64716e13aa02511a0e3074cf18ba493be22f
Fyodor Sizov <f.sizov@yandex.ru>
2018-01-17 16:51:36 +0300

    AmpEngine Log Object

--- log_handler/__init__.py
+++ log_handler/__init__.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
 
-
-class LogData:
-    def __init__(self):
-        self.logs = {}
-
-    def add_log(self, key, json_message):
-        self.logs[key] = json_message
-
-    def get_all_logs(self):
-        return self.logs
-
-    def get_log(self, key):
-        return self.logs[key]
-
-
-Logger = LogData()
+import log_handler.log_object as log_object
