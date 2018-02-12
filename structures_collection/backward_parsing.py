6df6ebd4da3fb8fc7d5c6e915d442b7e80e26016
Fyodor Sizov <f.sizov@yandex.ru>
2018-02-12 19:42:29 +0300

    fix dependencies & make iter

--- structures_collection/backward_parsing.py
+++ structures_collection/backward_parsing.py
@@ -35,7 +35,7 @@ def new_parser(parent_system, child_system):
 
 class SegmentForward:
     def __init__(self, container):
-        self.morpheme_maps = {}
+        self.__maps = {}
         self.container = container
 
     def generate_map(self, chars, start, dead_pos, position):
@@ -83,7 +83,7 @@ class SegmentForward:
                 perms = list(itertools.permutations(morpheme_pos, e + 1))
                 for perm in perms:
                     local_new_key = tuple(list(position) + [position_index])
-                    self.morpheme_maps[local_new_key] = {morph_object.get_id(): perm}
+                    self.__maps[local_new_key] = {morph_object.get_id(): perm}
 
                     local_dead_pos = dead_pos[:] + list(itertools.chain(*perm))
                     local_dead_pos = list(set(local_dead_pos))
@@ -103,14 +103,14 @@ class SegmentForward:
                     try:
                         self.generate_map(chars, start_integer, local_dead_pos, local_new_key)
                     except InternalParserException:
-                        self.morpheme_maps[tuple(list(local_new_key) + [0])] = None
+                        self.__maps[tuple(list(local_new_key) + [0])] = None
                     position_index += 1
 
         if not morpheme_found:
             raise InternalParserException()
 
     def get_result(self):
-        return self.morpheme_maps
+        return self.__maps
 
     @staticmethod
     def create_map_sequence(maps, key):
@@ -158,112 +158,118 @@ def morpheme_in_token(input_container_element, container, input_container):
         )
 
     # dead_pos should be specified during an iteration
-    segment_forward(input_container_element.get_content(), start=0, dead_pos=[], position=(0,))
-    morpho_seqs = []
-    for morph_key in morpheme_maps:
-        for group in reversed(morpheme_maps[morph_key][1]):
-            if group[-1] == len(input_container_element.get_content()) - 1:
-                morpho_seqs.append(create_morpho_sequence(morph_key))
-                break
+    for n, stem in enumerate(stems):
+        sf_object = SegmentForward(container)
+        # should be spec-dependent
+        sf_object.generate_map(
+            input_container_element.get_content(), start=0, dead_pos=stem.get_prop('positions'), position=(0,)
+        )
+        morpheme_maps = sf_object.get_result()
+        morpho_seqs = []
+        for morph_key in morpheme_maps:
+            for group in reversed(morpheme_maps[morph_key][1]):
+                if group[-1] == len(input_container_element.get_content()) - 1:
+                    morpho_seqs.append(sf_object.create_map_sequence(morpheme_maps, morph_key))
+                    break
 
-    # seq: List of Dict(a => b, c => d)
-    filtered_seqs = []
-    grammar_nulls = container.iter_content_filter(lambda x: x == grammar.Temp.NULL, system_filter='universal:morpheme')
-    for seq in morpho_seqs:
-        id_list = [container.get_by_id(list(x.keys())[0]) for x in seq]
-        available_nulls = []
-        for null in grammar_nulls:
-            for link in null.get_applied()['links']:
-                if link.check(input_container_element, id_list, lambda: True):
-                    available_nulls.append(null)
+        # seq: List of Dict(a => b, c => d)
+        filtered_seqs = []
+        grammar_nulls = container.iter_content_filter(lambda x: x == grammar.Temp.NULL, system_filter='universal:morpheme')
+        for seq in morpho_seqs:
+            id_list = [container.get_by_id(list(x.keys())[0]) for x in seq]
+            available_nulls = []
+            for null in grammar_nulls:
+                for link in null.get_applied()['links']:
+                    if link.check(input_container_element, id_list, lambda: True):
+                        available_nulls.append(null)
 
-        subcl_orders = container.get_system('universal:morpheme').get_subcl_orders_affecting_ids(id_list)
-        strict_prohib = False
-        for order in subcl_orders:
-            order_check = order.check_sequence(order, available_nulls)
-            if not order_check['check'] and order.is_strict():
-                strict_prohib = True
-            elif order.is_strict() and strict_prohib:
-                strict_prohib = False
+            subcl_orders = container.get_system('universal:morpheme').get_subcl_orders_affecting_ids(id_list)
+            strict_prohib = False
+            for order in subcl_orders:
+                order_check = order.check_sequence(order, available_nulls)
+                if not order_check['check'] and order.is_strict():
+                    strict_prohib = True
+                elif order.is_strict() and strict_prohib:
+                    strict_prohib = False
+                if strict_prohib:
+                    continue
+                if order_check['nulls']:
+                    for null in order_check['nulls']:
+                        if null['pre']:
+                            for pre in null['pre']:
+                                dec_value = decode_asterisk_pattern(pre)
+                                for j, seq_el in enumerate(seq):
+                                    if (dec_value[0] == 'id' and seq_el == dec_value[1]) or \
+                                    (dec_value[0] == 'class' and dec_value[1] in container.get_by_id(seq_el).get_class_names()):
+                                        prev_elem_pos = list(seq[j].keys())[0]
+                                        virtual_list = [-1, (prev_elem_pos[0] if prev_elem_pos[0] != -1 else prev_elem_pos[1])]
+                                        if j < len(seq) - 1:
+                                            next_elem_pos = list(seq[j + 1].keys())[0]
+                                            virtual_list.append(next_elem_pos[0] if next_elem_pos[0] != -1 else next_elem_pos[1])
+                                        seq.insert(
+                                            j + 1,
+                                            {
+                                                get_null_id(decode_asterisk_pattern(null['rx']), container): [virtual_list]
+                                            }
+                                        )
+                        elif null['post']:
+                            for post in null['post']:
+                                dec_value = decode_asterisk_pattern(post)
+                                for j, seq_el in enumerate(seq):
+                                    if j == 0:
+                                        continue
+                                    if (dec_value[0] == 'id' and seq_el == dec_value[1]) or \
+                                    (dec_value[0] == 'class' and dec_value[1] in container.get_by_id(seq_el).get_class_names()):
+                                        next_elem_pos = list(seq[j].keys())[0]
+                                        virtual_list = [-1, (next_elem_pos[0] if next_elem_pos[0] != -1 else next_elem_pos[1])]
+                                        if j > 0:
+                                            prev_elem_pos = list(seq[j - 1].keys())[0]
+                                            virtual_list.append(prev_elem_pos[0] if prev_elem_pos[0] != -1 else prev_elem_pos[1])
+                                        seq.insert(
+                                            j - 1,
+                                            {
+                                                get_null_id(decode_asterisk_pattern(null['rx']), container): [virtual_list]
+                                            }
+                                        )
+                        else:
+                            # UNALLOCATED
+                            ...
+
             if strict_prohib:
                 continue
-            if order_check['nulls']:
-                for null in order_check['nulls']:
-                    if null['pre']:
-                        for pre in null['pre']:
-                            dec_value = decode_asterisk_pattern(pre)
-                            for j, seq_el in enumerate(seq):
-                                if (dec_value[0] == 'id' and seq_el == dec_value[1]) or \
-                                (dec_value[0] == 'class' and dec_value[1] in container.get_by_id(seq_el).get_class_names()):
-                                    prev_elem_pos = list(seq[j].keys())[0]
-                                    virtual_list = [-1, (prev_elem_pos[0] if prev_elem_pos[0] != -1 else prev_elem_pos[1])]
-                                    if j < len(seq) - 1:
-                                        next_elem_pos = list(seq[j + 1].keys())[0]
-                                        virtual_list.append(next_elem_pos[0] if next_elem_pos[0] != -1 else next_elem_pos[1])
-                                    seq.insert(
-                                        j + 1,
-                                        {
-                                            get_null_id(decode_asterisk_pattern(null['rx'])): [virtual_list]
-                                        }
-                                    )
-                    elif null['post']:
-                        for post in null['post']:
-                            dec_value = decode_asterisk_pattern(post)
-                            for j, seq_el in enumerate(seq):
-                                if j == 0:
-                                    continue
-                                if (dec_value[0] == 'id' and seq_el == dec_value[1]) or \
-                                (dec_value[0] == 'class' and dec_value[1] in container.get_by_id(seq_el).get_class_names()):
-                                    next_elem_pos = list(seq[j].keys())[0]
-                                    virtual_list = [-1, (next_elem_pos[0] if next_elem_pos[0] != -1 else next_elem_pos[1])]
-                                    if j > 0:
-                                        prev_elem_pos = list(seq[j - 1].keys())[0]
-                                        virtual_list.append(prev_elem_pos[0] if prev_elem_pos[0] != -1 else prev_elem_pos[1])
-                                    seq.insert(
-                                        j - 1,
-                                        {
-                                            get_null_id(decode_asterisk_pattern(null['rx'])): [virtual_list]
-                                        }
-                                    )
-                    else:
-                        # UNALLOCATED
-                        ...
 
-        if strict_prohib:
-            continue
+            filtered_seqs.append(seq)
 
-        filtered_seqs.append(seq)
-
-    lnk_filtered_seqs = []
-    for seq in filtered_seqs:
-        add_to_lfs = True
-        el_seq = [container.get_by_id(list(x.keys())[0]) for x in seq]
-        for mc_element in el_seq:
-            for link in mc_element.get_applied()['links']:
-                if not link.check(input_container_element, el_seq, lambda: True):
-                    add_to_lfs = False
+        lnk_filtered_seqs = []
+        for seq in filtered_seqs:
+            add_to_lfs = True
+            el_seq = [container.get_by_id(list(x.keys())[0]) for x in seq]
+            for mc_element in el_seq:
+                for link in mc_element.get_applied()['links']:
+                    if not link.check(input_container_element, el_seq, lambda: True):
+                        add_to_lfs = False
+                        break
+                if not add_to_lfs:
                     break
-            if not add_to_lfs:
-                break
-        if add_to_lfs:
-            lnk_filtered_seqs.append(seq)
+            if add_to_lfs:
+                lnk_filtered_seqs.append(seq)
 
-    for j, seq in enumerate(lnk_filtered_seqs):
-        co_list = []
-        for seq_element in seq:
-            element_id = list(seq_element.keys())[0]
-            co_local = structures_collection.char_level.CharOutline(
-                [],
-                attachment=container.get_by_id(element_id).get_content(),
-                metadata={'mc_id': element_id}
-            )
-            for ci in seq_element[element_id]:
-                if ci[0] != -1:
-                    co_local.add_group(structures_collection.char_level.CharIndexGroup(ci))
-                else:
-                    co_local.add_group(structures_collection.char_level.CharIndexGroup(ci[1:], is_virtual=True))
+        for j, seq in enumerate(lnk_filtered_seqs):
+            co_list = []
+            for seq_element in seq:
+                element_id = list(seq_element.keys())[0]
+                co_local = structures_collection.char_level.CharOutline(
+                    [],
+                    attachment=container.get_by_id(element_id).get_content(),
+                    metadata={'mc_id': element_id}
+                )
+                for ci in seq_element[element_id]:
+                    if ci[0] != -1:
+                        co_local.add_group(structures_collection.char_level.CharIndexGroup(ci))
+                    else:
+                        co_local.add_group(structures_collection.char_level.CharIndexGroup(ci[1:], is_virtual=True))
 
-        input_container.segment_element(input_container_element, 'universal:morpheme', co_list, set_group=j)
+            input_container.segment_element(input_container_element, 'universal:morpheme', co_list, set_group=n+j)
 
 
 class ParserNotFound(Exception):
