0d8f2a15eee57c60d06ca5aa100cab9423cedec8
Fyodor Sizov <f.sizov@yandex.ru>
2018-02-13 00:37:55 +0300

    +output templates (draft)

---
+++ output_templates/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+#from output_templates import
---
+++ output_templates/mansi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Template:
+    def __init__(self, container):
+        self.container = container
+
+    def run(self, input_container, **kwargs):
+        pass
