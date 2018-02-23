d0ea32348103d27dde17d8b68852bc08ead0744c
Fyodor Sizov <f.sizov@yandex.ru>
2018-02-24 00:38:22 +0300

    mansi output template (upd)

--- output_templates/mansi.py
+++ output_templates/mansi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import grammar
+from output_templates import lgr_action as lgr
 import json
 
 
@@ -17,11 +18,27 @@ class Template:
             for token in tokens:
                 print("Token: %s" % (token.get_content()))
                 token_gc = grammar.GroupCollection(
-                    token.get_childs(lambda tkn: tkn.get_system_name() == 'universal:morpheme')
+                    token.get_childs(lambda tkn: tkn.get_system_name() == 'universal:morpheme'),
+                    token.get_ic_id(),
+                    input_container
                 )
                 for group in token_gc.groups():
-                    for element in group:
-                        ...
+                    # get stem and print it
+                    group_text = ""
+                    group_markers = ""
+                    for morpheme in group:
+                        group_text += "-" + morpheme.get_content()
+                        morpheme_markers = []
+                        for action in self.container.get_by_id(morpheme.mc_id_link).get_actions():
+                            morpheme_markers.append(
+                                lgr.lgr_values[action](action) if action.get_path() in lgr.lgr_values else '?'
+                            )
+                        group_markers += "-" + ".".join(morpheme_markers)
+                    print("Group:")
+                    print(group_text)
+                    print(group_markers)
+
+
 
 
 class InputContainerIsEmpty(Exception):
