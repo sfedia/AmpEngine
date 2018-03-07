8e79662737472e71f84cab1aabc9fb88f6f5a983
Fyodor Sizov <f.sizov@yandex.ru>
2018-03-07 14:18:53 +0300

    update Conversion + ConvAction

--- structures_collection/co_conversion.py
+++ structures_collection/co_conversion.py
@@ -35,22 +35,62 @@ Handler = HandlerStart()
 
 class Conversion:
     def __init__(self):
-        self.__match_from = {}
+        self.__match_fc = {}
 
-    def add_match(self, a, b, regex_before=(0, None), before_strict=False, regex_after=(0, None), after_strict=False):
-        mf_key = tuple(x for x in a)
-        if mf_key not in self.__match_from:
-            self.__match_from[mf_key] = {}
-        self.__match_from[mf_key][(regex_before, regex_after)] = b
+    def add_match(self, a, b, regex_before=(0, None), regex_after=(0, None)):
+        if not a:
+            raise ValueError()
+        if a[0] not in self.__match_fc:
+            self.__match_fc[a[0]] = {}
+        self.__match_fc[a[0]][(regex_before, a, regex_after)] = b
 
-    def char_replace(self, char, before=[], after=[]):
-        return [
-            self.__match_from[char][x] for x in self.__match_from[char]
-            if x[0][0] == len(before) and re.search(x[0][1], ''.join(before))
-            and x[1][0] == len(after) and re.search(x[1][1], ''.join(after))
-        ]
+    def char_request(self, index, co_sequence):
+        for properties in self.__match_fc[co_sequence[index]]:
+            if properties[2][0] < len(co_sequence[index + 1:]):
+                continue
+            elif not re.search(properties[2][1], co_sequence[index+1:index+1+properties[2][0]]):
+                continue
+            if properties[0][0] < len(co_sequence[:index]):
+                continue
+            elif not re.search(properties[0][1], co_sequence[index-properties[0][1]:index]):
+                continue
+
+            if index + len(properties[1]) >= len(co_sequence):
+                continue
+
+            return ConvAction(
+                list(range(index, index + len(properties[1]))),
+                [x for x in self.__match_fc[co_sequence[index]][properties]]
+            )
+
+        return ConvAction('Equal', [index], [co_sequence[index]])
 
 
+class ConvAction:
+    def __init__(self, int_list, rep_list, action_type=None):
+        if action_type is None:
+            if len(rep_list) - len(int_list) > 0:
+                self.__action_type = 'Divide'
+            elif len(rep_list) == len(int_list):
+                self.__action_type = 'Equal'
+            elif len(rep_list) - len(int_list) < 0:
+                self.__action_type = 'Merge'
+        elif action_type not in ('Equal', 'Divide', 'Merge'):
+            raise ValueError
+        self.__action_type = action_type
+        self.__int_list = int_list
+        self.__rep_list = rep_list
+
+    def what(self):
+        return self.__action_type
+
+    def get(self, shift=0):
+        return (
+            self.__action_type,
+            [x + shift for x in self.__int_list],
+            self.__rep_list,
+            len(self.__rep_list) - len(self.__int_list)
+        )
 
 
 def regressive_conversion(from_, to_):
