00b85ab0bbc70ee1a8625981f28cecc3f1316e2e
Fyodor Sizov <f.sizov@yandex.ru>
2018-03-31 02:31:52 +0300

    + structures_collection.minor

--- structures_collection/__init__.py
+++ structures_collection/__init__.py
@@ -7,3 +7,4 @@ import structures_collection.static as static
 import structures_collection.sharp_function as sharp_function
 import structures_collection.layer_extraction as layer_extraction
 import structures_collection.system_multirendering as system_multirendering
+import structures_collection.minor as minor
