05e671fdfc2672c9147a67e6d7172b2d67d25871
Fyodor Sizov <f.sizov@yandex.ru>
2018-03-31 02:23:29 +0300

    + structures_collection.minor

---
+++ structures_collection/minor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Clear:
+    @staticmethod
+    def remove_spec_chars(system_name, content):
+        if system_name == 'universal:morpheme':
+            if content.startswith('^'):
+                return content[1:]
