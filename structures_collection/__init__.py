0f46c41b66b9ed1c2330637726c22f9e17e88ad8
Fyodor Sizov <f.sizov@yandex.ru>
2018-04-05 00:49:21 +0300

    + .backward_parsing

--- structures_collection/__init__.py
+++ structures_collection/__init__.py
@@ -5,6 +5,7 @@ import structures_collection.dependency as dependency
 import structures_collection.auto_parameter_extraction as auto_parameter_extraction
 import structures_collection.static as static
 import structures_collection.sharp_function as sharp_function
+import structures_collection.backward_parsing as backward_parsing
 import structures_collection.layer_extraction as layer_extraction
 import structures_collection.system_multirendering as system_multirendering
 import structures_collection.minor as minor
