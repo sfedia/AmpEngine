639e83071edb4d96692661cb120268e875d7f42c
Fyodor Sizov <f.sizov@yandex.ru>
2018-04-06 15:16:56 +0300

    update dependency

--- structures_collection/dependency.py
+++ structures_collection/dependency.py
@@ -2,25 +2,13 @@
 
 systems = {
     'universal:input': [
-        'universal:token',
-        'universal:collocation'
+        'universal:sentence'
     ],
-    'universal:collocation': [
+    'universal:sentence': [
         'universal:token'
     ],
     'universal:token': [
-        'universal:morpheme',
-        'mansi:morphemeYU',
-        'mansi:VowMorpheme',
-        'universal:char',
-        'universal:char_regex'
+        'universal:morpheme'
     ],
-    'universal:morpheme': [
-        'universal:char',
-        'universal:char_regex'
-    ],
-    'mansi:VowMorpheme': ['universal:morpheme'],
-    'mansi:morphemeYU': ['universal:morpheme'],
-    'universal:char': [],
-    'universal:char_regex': ['universal:char']
+    'universal:morpheme': []
 }
