da313912d33f25ef6ddeee5477670bbf692c5738
Fyodor Sizov <f.sizov@yandex.ru>
2018-04-18 14:13:18 +0300

    fix the bug in .is_last_in_cluster(...)

--- grammar.py
+++ grammar.py
@@ -267,7 +267,7 @@ class InputContainerElement:
 
     def is_last_in_cluster(self):
         p_childs = self.input_container.get_by_ic_id(self.get_parent_ic_id()).get_childs()
-        return self in p_childs and p_childs.index(self) == len(p_childs)
+        return self in p_childs and p_childs.index(self) == len(p_childs) - 1
 
     def co_follows(self, index):
         fg = self.char_outline.get_groups()[0]
