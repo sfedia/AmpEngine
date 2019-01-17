7987b2e943c4f898b83cd9dd272dada762464eae
Fyodor Sizov <f.sizov@yandex.ru>
2019-01-17 04:24:43 +0300

    updated backward_parsing

--- structures_collection/backward_parsing.py
+++ structures_collection/backward_parsing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from collections import namedtuple
 import itertools
 import grammar
 import structures_collection.char_level
@@ -38,97 +39,141 @@ def new_parser(parent_system, child_system):
     return parser_decorator
 
 
-class SegmentForward:
-    def __init__(self, container):
-        self.__maps = {}
+bp_cache = {
+    "universal:morpheme_by_init_char": {},
+    "grammar_nulls": []
+}
+
+
+class IterativeSingleSegmentation:
+    def __init__(self, container, input_container_element, frame_string, co_start):
         self.container = container
+        self.input_container_element = input_container_element
+        self.frame_string = frame_string
+        self.co_start = co_start
+        self.elements_on_id = dict()
 
-    def generate_map(self, chars, start, dead_pos, position):
-        while start in dead_pos:
-            start += 1
+    def generate_char_outlines(self, left_id_sequence):
+        co_list = []
+        for element_id in left_id_sequence:
+            co_local = structures_collection.char_level.CharOutline(
+                [],
+                attachment=self.elements_on_id[element_id].get_clear_content(),
+                metadata={'mc_id': element_id}
+            )
+            if self.elements_on_id[element_id].get_content() == grammar.Temp.NULL:
+                co_local.add_group(
+                    structures_collection.char_level.CharIndexGroup(
+                        [self.co_start, self.co_start + len(self.elements_on_id[element_id].get_content())]
+                    )
+                )
+                self.co_start += len(self.elements_on_id[element_id].get_content())
+            else:
+                co_local.add_group(
+                    structures_collection.char_level.CharIndexGroup(
+                        [-1, -1], is_virtual=True
+                    )
+                )
+            co_list.append(co_local)
+        return co_list
 
-        if start >= len(chars):
-            raise InternalParserException()
+    def start(self):
+        try:
+            return self.continue_sequence([], 0)
+        except SegmentationIterFailed:
+            return None
 
-        morphemes = self.container.iter_content_filter(
-            lambda x: mnr.Clear.remove_spec_chars('universal:morpheme', x).startswith(chars[start]),
-            sort_desc=True,
-            system_filter='universal:morpheme'
-        )
-        if not morphemes:
-            raise InternalParserException()
+    def continue_sequence(self, left_id_sequence, char_index):
+        if char_index >= len(self.frame_string):
+            csc = self.common_sequence_check(left_id_sequence)
+            if csc:
+                return csc
+            else:
+                raise SegmentationIterFailed()
+        else:
+            if self.frame_string[char_index] not in bp_cache["universal:morpheme_by_init_char"]:
+                next_options = bp_cache["universal:morpheme_by_init_char"][self.frame_string[char_index]]
+            else:
+                next_options = self.container.iter_content_filter(
+                    lambda x: mnr.Clear.remove_spec_chars('universal:morpheme', x).startswith(self.frame_string[char_index]),
+                    sort_desc=True,
+                    system_filter='universal:morpheme'
+                )
+                bp_cache["universal:morpheme_by_init_char"][self.frame_string[char_index]] = next_options
 
-        morpheme_found = False
-        position_index = 0
-
-        for morph_object in morphemes:
-            morpheme_pos = []
-            catch_pos = []
-
-            for j in range(start, len(chars)):
-                if j in dead_pos:
+            for option in next_options:
+                option_id = option.get_ic_id()
+                self.elements_on_id[option_id] = option
+                try:
+                    return self.continue_sequence(
+                        left_id_sequence + [option.get_ic_id()],
+                        char_index + len(mnr.Clear.remove_spec_chars('universal:morpheme', option.get_content()))
+                    )
+                except SegmentationIterFailed:
                     continue
-                if (j - start) == len(morph_object.get_clear_content()):
-                    break
 
-                mcnt = morph_object.get_clear_content()
-                if len(catch_pos) < len(mcnt) and chars[j] == mcnt[j - start]:
-                    catch_pos.append(j)
-                elif len(catch_pos) < len(morph_object.get_clear_content()):
-                    catch_pos = []
-                elif len(catch_pos) == len(morph_object.get_clear_content()):
-                    morpheme_pos.append(catch_pos)
-                    catch_pos = []
-                else:
-                    raise InternalParserException()
+    def common_sequence_check(self, id_sequence):
+        if not self.check_sequence(id_sequence):
+            return False
+        avnulls = self.check_avnulls_of_sequence(id_sequence)
+        orders = self.container.get_system('universal:morpheme').get_subcl_orders_affecting_ids(id_sequence)
+        for order in orders:
+            order_check = self.check_sequence_by_order(id_sequence, order, avnulls)
+            if not order_check.order_works:
+                if order_check.strict:
+                    return False
+            else:
+                id_sequence = order_check.sequence
+        return id_sequence
 
-            if len(catch_pos) == len(morph_object.get_clear_content()):
-                morpheme_pos.append(catch_pos)
+    def check_sequence(self, id_sequence):
+        el_seq = [self.elements_on_id[x] for x in id_sequence]
+        bses = el_seq
+        for j, elem in enumerate(el_seq):
+            elem.set_transmitter_local_index(j)
+        for j, elem in enumerate(el_seq):
+            for link in elem.get_applied()['links']:
+                lc_bool, bses, self.input_container_element = link.check(
+                    self.input_container_element, bses, lambda x: True, return_bs=True
+                )
+                if not lc_bool:
+                    return False
 
-            if not morpheme_pos:
-                continue
-            morpheme_found = True
+    def check_avnulls_of_sequence(self, id_sequence):
+        el_seq = [self.elements_on_id[x] for x in id_sequence]
+        for e, elem in enumerate(el_seq):
+            elem.set_transmitter_local_index(e)
+        available_nulls = []
+        if "grammar_nulls" not in bp_cache:
+            bp_cache["grammar_nulls"] = self.container.iter_content_filter(
+                lambda x: x == grammar.Temp.NULL, system_filter='universal:morpheme'
+            )
+        grammar_nulls = bp_cache["grammar_nulls"]
+        bsid_obj = el_seq + grammar_nulls
+        for e, null in enumerate(grammar_nulls):
+            null.set_transmitter_local_index(len(el_seq) + e)
+        for e, null in enumerate(grammar_nulls):
+            for link in null.get_applied()['links']:
+                # BS Array
+                lc_bool, bsid_obj, input_container_element = link.check(
+                    self.input_container_element, bsid_obj, lambda x: True, return_bs=True
+                )
+                if lc_bool:
+                    available_nulls.append(null)
+        return available_nulls
 
-            for e in range(len(morpheme_pos)):
-                perms = list(itertools.permutations(morpheme_pos, e + 1))
-                for perm in perms:
-                    local_new_key = position + (position_index,)
-                    self.__maps[local_new_key] = {morph_object.get_id(): perm}
+    def check_sequence_by_order(self, id_sequence, order, available_nulls):
+        result = namedtuple("result", ["order_works", "strict", "sequence"])
+        el_seq = [self.elements_on_id[x] for x in id_sequence]
+        order_check = order.check_sequence(el_seq, available_nulls)
+        return result(order_check["check"], order.is_strict(), order_check["cn_sequence"])
 
-                    local_dead_pos = dead_pos[:] + list(itertools.chain(*perm))
-                    local_dead_pos = list(set(local_dead_pos))
-                    local_dead_pos.sort()
-
-                    start_integer = None
-                    for ii, i in enumerate(local_dead_pos):
-                        if not ii:
-                            continue
-                        if local_dead_pos[ii - 1] - local_dead_pos[ii] > 1:
-                            start_integer = local_dead_pos[ii - 1] + 1
-                            break
-
-                    if not start_integer:
-                        start_integer = local_dead_pos[-1] + 1
-
-                    try:
-                        self.generate_map(chars, start_integer, local_dead_pos, local_new_key)
-                    except InternalParserException:
-                        self.__maps[local_new_key + (0,)] = None
-                    position_index += 1
-
-        if not morpheme_found:
-            raise InternalParserException()
-
-    def get_result(self):
-        return self.__maps
-
-    @staticmethod
-    def create_map_sequence(maps, key):
-        sequence = []
-        while len(key) >= 2:
-            sequence.insert(0, maps[key])
-            key = tuple(x for x in key[:-1])
-        return sequence
+    def check_prw_of_sequence(self, id_sequence):
+        act_list = [self.container.get_by_id(x).get_applied()['actions'] for x in id_sequence]
+        act_list = list(itertools.chain(*act_list))
+        params_list = [structures_collection.static.Handler.get_func_params(x.get_path()) for x in act_list]
+        params_list = list(itertools.chain(*params_list))
+        return len(params_list) == len(set(params_list))
 
 
 def decode_asterisk_pattern(pattern):
@@ -165,132 +210,18 @@ def morpheme_in_token(input_container_element, container, input_container):
     for n, stem in enumerate(stems):
         if len(stem.get_prop('positions')) >= len(input_container_element.get_content()):
             continue
-        sf_object = SegmentForward(container)
-        # should be spec-dependent
-        sf_object.generate_map(
-            input_container_element.get_content(), start=0, dead_pos=stem.get_prop('positions'), position=(0,)
+        co_start = stem.get_prop('positions')[-1] + 1
+        iss_instance = IterativeSingleSegmentation(
+            container,
+            input_container_element,
+            input_container_element.get_content()[co_start:],
+            co_start
         )
-        morpheme_maps = sf_object.get_result()
-        morpho_seqs = []
-        for morph_key in morpheme_maps:
-            if morpheme_maps[morph_key] is None:
-                continue
-            [(v, group_holder)] = morpheme_maps[morph_key].items()
-            for group in reversed(group_holder):
-                if group[-1] == len(input_container_element.get_content()) - 1:
-                    morpho_seqs.append(sf_object.create_map_sequence(morpheme_maps, morph_key))
-                    break
-
-        # seq: List of Dict(a => b, c => d)
-        filtered_seqs = []
-        grammar_nulls = container.iter_content_filter(lambda x: x == grammar.Temp.NULL, system_filter='universal:morpheme')
-        for seq in morpho_seqs:
-            id_list = [container.get_by_id(list(x.keys())[0]) for x in seq]
-            for e, elem in enumerate(id_list):
-                elem.set_transmitter_local_index(e)
-            available_nulls = []
-            bsid_obj = id_list + grammar_nulls
-            for e, null in enumerate(grammar_nulls):
-                null.set_transmitter_local_index(len(id_list) + e)
-            for e, null in enumerate(grammar_nulls):
-                for link in null.get_applied()['links']:
-                    # BS Array
-                    lc_bool, bsid_obj, input_container_element = link.check(
-                        input_container_element, bsid_obj, lambda x: True, return_bs=True
-                    )
-                    if lc_bool:
-                        available_nulls.append(null)
-
-            subcl_orders = container.get_system('universal:morpheme').get_subcl_orders_affecting_ids([
-                el.get_id() for el in id_list
-            ])
-            strict_prohib = False
-            for order in subcl_orders:
-                order_check = order.check_sequence(id_list, available_nulls)
-                if not order_check['check'] and order.is_strict():
-                    strict_prohib = True
-                elif order.is_strict() and strict_prohib:
-                    strict_prohib = False
-                if strict_prohib:
-                    continue
-
-                cn_seq = order_check['cn_sequence']
-                upd_seq = []
-
-                real_elements = [j for j, el in enumerate(cn_seq) if el.get_content() != grammar.Temp.NULL]
-                nc = 0
-                for j, element in enumerate(cn_seq):
-                    if element.get_content() == grammar.Temp.NULL:
-                        nc -= 1
-                        if j > 0:
-                            for k in upd_seq[j + nc]:
-                                upd_seq.append({element.get_id(): ([-1, upd_seq[j + nc][k][0][0]],)})
-                        elif j < len(upd_seq) - 1:
-                            for k in upd_seq[j + 1]:
-                                upd_seq.append({element.get_id(): ([-1, upd_seq[j + nc][k][0][0]],)})
-                    else:
-                        rei = real_elements.index(j)
-                        for k in seq[rei]:
-                            upd_seq.append({element.get_id(): seq[rei][k]})
-
-                seq = upd_seq
-            if strict_prohib:
-                continue
-
-            filtered_seqs.append(seq)
-
-        lnk_filtered_seqs = []
-        for seq in filtered_seqs:
-            add_to_lfs = True
-            el_seq = [container.get_by_id(list(x.keys())[0]) for x in seq]
-            bses = el_seq
-            for j, elem in enumerate(el_seq):
-                elem.set_transmitter_local_index(j)
-            for j, elem in enumerate(el_seq):
-                for link in elem.get_applied()['links']:
-                    lc_bool, bses, input_container_element = link.check(
-                        input_container_element, bses, lambda x: True, return_bs=True
-                    )
-                    if not lc_bool:
-                        add_to_lfs = False
-                        break
-                if not add_to_lfs:
-                    break
-            if add_to_lfs:
-                lnk_filtered_seqs.append(seq)
-
-        prw_filtered_seqs = []
-        if input_container.config.param_rewrite:
-            prw_filtered_seqs = lnk_filtered_seqs
-        else:
-            for seq in lnk_filtered_seqs:
-                idl = [list(x.keys())[0] for x in seq]
-                act_list = [container.get_by_id(x).get_applied()['actions'] for x in idl]
-                act_list = list(itertools.chain(*act_list))
-                params_list = [structures_collection.static.Handler.get_func_params(x.get_path()) for x in act_list]
-                params_list = list(itertools.chain(*params_list))
-                if len(params_list) == len(set(params_list)):
-                    prw_filtered_seqs.append(seq)
-
-        for j, seq in enumerate(prw_filtered_seqs):
-            co_list = []
-            for seq_element in seq:
-                element_id = list(seq_element.keys())[0]
-                co_local = structures_collection.char_level.CharOutline(
-                    [],
-                    attachment=container.get_by_id(element_id).get_clear_content(),
-                    metadata={'mc_id': element_id}
-                )
-                for ci in seq_element[element_id]:
-                    if ci[0] != -1:
-                        co_local.add_group(structures_collection.char_level.CharIndexGroup(ci))
-                    else:
-                        co_local.add_group(structures_collection.char_level.CharIndexGroup(ci[1:], is_virtual=True))
-                co_list.append(co_local)
-
-            input_container.segment_element(input_container_element, 'universal:morpheme', co_list, set_group=group_index)
-            group_index += 1
-
+        markup = iss_instance.start()
+        markup_outlines = iss_instance.generate_char_outlines(markup)
+        input_container.segment_element(
+            input_container_element, 'universal:morpheme', markup_outlines, set_group=group_index
+        )
         group_index += 1
 
 
@@ -299,4 +230,8 @@ class ParserNotFound(Exception):
 
 
 class InternalParserException(Exception):
+    pass
+
+
+class SegmentationIterFailed(Exception):
     pass
